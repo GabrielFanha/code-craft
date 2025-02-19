0a94c61bfcf10a2c51e3e17915bc7fe829c9537a
Gabriel Fanha <gabriel.lfanha@outlook.com>
2025-02-18 21:30:56 -0300

    create invite page and config components

---
+++ src/app/invite/invite-link-input.tsx
@@ -0,0 +1,28 @@
+'use client'
+
+import { IconButton } from '@/components/icon-button'
+import { InputField, InputIcon, InputRoot } from '@/components/input'
+import { Copy, Link } from 'lucide-react'
+
+interface inviteLinkProps {
+  inviteLink: string
+}
+
+export function InviteLinkInput({ inviteLink }: inviteLinkProps) {
+  function copyInviteLink() {
+    navigator.clipboard.writeText(inviteLink)
+  }
+
+  return (
+    <InputRoot>
+      <InputIcon>
+        <Link className="size-5" />
+      </InputIcon>
+      <InputField readOnly defaultValue={inviteLink} />
+
+      <IconButton className="-mr-2">
+        <Copy className="size-5" onClick={copyInviteLink} />
+      </IconButton>
+    </InputRoot>
+  )
+}
